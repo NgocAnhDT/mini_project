cfc8b59c2879bc1fceb8779f71c796a9e13b71a6
Ha Thanh Hoang <Hathanhhoang842000@gmail.com>
2022-01-21 09:54:02 +0700

    hthoang commit

--- backend/views/products/detail.php
+++ backend/views/products/detail.php
@@ -1,1 +1,51 @@
-<?php
+
+
+<div class="container-fluid">
+    <h2>Thông tin sản phẩm</h2>
+    <?php if($this->error != ''): ?>
+        <div class="noti-error">
+            <p>
+                <?php echo $this->error; ?>
+            </p>
+        </div>
+    <?php endif; ?>
+        <div class="title text-center">
+            Sản phẩm #<?php echo $product['product_id']; ?>
+        </div>
+    <table id="pro-inf">
+        <tr class="row">
+            <td class="col-lg-3 text-right name-label">Tên sản phẩm</td>
+            <td class="col-lg-8 name-product"><?php echo $product['product_name']; ?></td>
+        </tr>
+        <tr class="row">
+            <td class="col-lg-3 text-right img-label">Ảnh sản phẩm</td>
+            <td class="col-lg-8 img">
+                <img src="assets/images/uploads/<?php echo $product['product_img']; ?>" alt="<?php echo $product['product_name']; ?>" class="img-rounded">
+            </td>
+        </tr>
+        <tr class="row">
+            <td class="col-lg-3 text-right brand-label">Thương hiệu</td>
+            <td class="col-lg-8 brand"><?php echo $product['brand']; ?></td>
+        </tr>
+        <tr class="row">
+            <td class="col-lg-3 text-right price-label">Giá bán</td>
+            <td class="col-lg-8 price"><?php echo number_format($product['price'],0, '.', ' ') ?><sup>đ</sup></td>
+        </tr>
+        <tr class="row">
+            <td class="col-lg-3 text-right size-label">Size</td>
+            <td class="col-lg-8 size"><?php echo $product['size']; ?></td>
+        </tr>
+        <tr class="row">
+            <td class="col-lg-3 text-right color-label">Màu sắc</td>
+            <td class="col-lg-8 color"><?php echo $product['color']; ?></td>
+        </tr>
+    </table>
+
+    <div class="status">
+        <a href="index.php?controller=product&action=update&id=<?php echo $product['product_id'] ?>" class="btn btn-warning">Sửa</a>
+        <a href="index.php?controller=product&action=delete&id=<?php echo $product['product_id'] ?>" onclick="return confirm('Chắc chắn muốn xóa sản phẩm này?')" class="btn btn-danger">Xóa</a>
+    </div>
+    <div class="back">
+        <a href="index.php?controller=product&action=index">Về trang danh sách sản phẩm</a>
+    </div>
+</div>
--- backend/views/products/index.php
+++ backend/views/products/index.php
@@ -1,1 +1,29 @@
-<?php
+
+<div class="container-fluid">
+    <h2>Danh sách sản phẩm</h2>
+    <a href="index.php?controller=product&action=create" class="btn btn-success">Thêm sản phẩm mới</a>
+    <div class="products row">
+        <?php foreach ($products as $product) : ?>
+            <div class="col-lg-3 col-md-6 col-sm-12 text-center mr-tb-20">
+                <div class="product">
+                    <div class="product-image">
+                        <a href="index.php?controller=product&action=detail&id=<?php echo $product['product_id'] ?>">
+                            <img src="assets/images/uploads/<?php echo $product['product_img'] ?>" alt="<?php echo $product['product_name'] ?>">
+                        </a>
+                    </div>
+                    <div class="product-name">
+                        <?php echo $product['product_name'] ?>
+                    </div>
+                    <div class="product-price">
+                        <?php echo number_format($product['price'],0,'.', ' '); ?><sup>đ</sup>
+                    </div>
+                    <div class="status">
+                        <a href="index.php?controller=product&action=detail&id=<?php echo $product['product_id'] ?>" class="btn btn-info">Chi tiết</a>
+                        <a href="index.php?controller=product&action=update&id=<?php echo $product['product_id'] ?>" class="btn btn-warning">Sửa</a>
+                        <a href="index.php?controller=product&action=delete&id=<?php echo $product['product_id'] ?>" onclick="return confirm('Chắc chắn muốn xóa sản phẩm này?')" class="btn btn-danger">Xóa</a>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
