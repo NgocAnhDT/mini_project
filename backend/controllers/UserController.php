e49e1a0ac9379db19dbe919559f4651bdd940028
NgocAnhDT <70968535+NgocAnhDT@users.noreply.github.com>
2022-01-26 23:53:44 +0700

    update remember

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -4,7 +4,6 @@ require_once 'controllers/Controller.php';
 require_once 'models/User.php';
 
 class UserController extends Controller {
-
     public function login() {
         // - Gán thông tin cụ thể theo chức năng hiện tại
         $this->page_title = "Login";
@@ -12,7 +11,7 @@ class UserController extends Controller {
         $userModel = new User();
 
         if(isset($_COOKIE['username']) && isset($_COOKIE['password'])) {
-            $row = $userModel->check_login($_COOKIE['username'], $_COOKIE['password']);
+            $row = $userModel->check_user($_COOKIE['username'], $_COOKIE['password']);
             if($row){
                 $_SESSION['logged'] = true;
                 $_SESSION['username'] = $username;
@@ -20,43 +19,42 @@ class UserController extends Controller {
                 header("Location: index.php");
                 exit();
             }
-        }
-
-        if (isset($_POST['btn_login'])){
-            if(empty($_POST['username'])){
-                $msg_error['username']="Hãy nhập username";
-            } elseif (strlen($_POST['username']) < 4){
-                $msg_error['username']="Hãy nhập username tối thiểu 4 kí tự";
-            } else {
-                $username = $_POST['username'];
-            }
-
-            if (empty($_POST['password'])){
-                $msg_error['password']="Hãy nhập password";
-            } elseif (strlen($_POST['password']) < 6) {
-                $msg_error['password'] = "Hãy nhập password tối thiểu 6 kí tự";
-            } else {
-                $pass = $_POST['password'];
-            }
-
-            if (!empty($username) && !empty($pass)) {
-                $row = $userModel->check_login($username, $pass);
-                if($row){
-                    $time_expire = 60*60*24*10;
-                    if(isset($_POST['remember'])){
-                        setcookie('username', $username, time()+ $time_expire);
-                        setcookie('password', $pass, time() + $time_expire);
-                    }
-                    $_SESSION['logged'] = true;
-                    $_SESSION['username'] = $username;
-                    $_SESSION['fullname'] = $row['fullname'];
-                    header("Location: index.php");
+        } else {
+            if (isset($_POST['btn_login'])){
+                if(empty($_POST['username'])){
+                    $msg_error['username']="Hãy nhập username";
+                } elseif (strlen($_POST['username']) < 4){
+                    $msg_error['username']="Hãy nhập username tối thiểu 4 kí tự";
                 } else {
-                    $msg_error[] = 'username hoặc password không đúng, vui lòng đăng nhập lại';
+                    $username = $_POST['username'];
+                }
+
+                if (empty($_POST['password'])){
+                    $msg_error['password']="Hãy nhập password";
+                } elseif (strlen($_POST['password']) < 6) {
+                    $msg_error['password'] = "Hãy nhập password tối thiểu 6 kí tự";
+                } else {
+                    $pass = $_POST['password'];
+                }
+
+                if (!empty($username) && !empty($pass)) {
+                    $row = $userModel->check_user($username, $pass);
+                    if($row){
+                        $time_expire = 60*60*24*10;  //10 days
+                        if(isset($_POST['remember'])){
+                            setcookie('username', $username, time()+ $time_expire);
+                            setcookie('password', $pass, time() + $time_expire);
+                        }
+                        $_SESSION['logged'] = true;
+                        $_SESSION['username'] = $username;
+                        $_SESSION['fullname'] = $row['fullname'];
+                        header("Location: index.php");
+                    } else {
+                        $msg_error[] = 'username hoặc password không đúng, vui lòng đăng nhập lại';
+                    }
                 }
             }
         }
-
         if(!empty($msg_error)){
             $this->error= $msg_error;
         }
@@ -76,11 +74,8 @@ class UserController extends Controller {
             setcookie('username','', time() - 1);
             setcookie('password','', time()- 1);
         }
-
         $_SESSION['success'] = "Đăng xuất thành công";
         header("Location: index.php?controller=user&action=login");
         exit();
     }
-
-
 }
